8f41fcfb6cf54965b19f08952c551509ed4d13b1
Sivasothy-Tharsa <tharsatharsi2000@gmail.com>
2025-03-09 21:55:51 +0530

    Credential update

--- backend/server.js
+++ backend/server.js
@@ -40,17 +40,22 @@ const s3Client = new S3Client({
 
 const BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
-let credentials;
-try {
-  credentials = JSON.parse(process.env.GOOGLE_SHEETS_KEY_FILE);
-} catch (error) {
-  console.error('Error parsing GOOGLE_SHEETS_KEY_FILE:', error);
-  throw new Error('Invalid Google Sheets credentials.');
-}
 
 // Google Sheets Configuration
 const auth = new google.auth.GoogleAuth({
-  keyFile: credentials,
+  keyFile: {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix newline characters
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
