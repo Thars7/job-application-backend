a345d130277d8ed99ee8b2c17942bc77661a766f
Sivasothy-Tharsa <tharsatharsi2000@gmail.com>
2025-03-10 22:39:30 +0530

    credential

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,19 @@ const BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
 // Google Sheets API Configuration
 const auth = new google.auth.GoogleAuth({
-  keyFile: process.env.GOOGLE_SHEETS_KEY_FILE,
+  credentials: {
+        type: process.env.GOOGLE_TYPE,
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix newline characters
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: process.env.GOOGLE_AUTH_URI,
+        token_uri: process.env.GOOGLE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+        universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+      },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
